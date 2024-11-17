a0b4a92e45b268888d64fa3735e52101fc440f4b
ChanwooCho <41471223+ChanwooCho@users.noreply.github.com>
2024-11-17 17:42:53 +0900

    Update llama.cpp

--- src/llama.cpp
+++ src/llama.cpp
@@ -107,8 +107,8 @@
 #define LLAMA_MAX_NODES   8192
 #define LLAMA_MAX_EXPERTS 160
 
-#define NUM_ATTN_HEAD 18
-#define NUM_KV_HEAD 9
+#define NUM_ATTN_HEAD 32
+#define NUM_KV_HEAD 8
 
 //
 // logging
